3cb4b4dbf0e662436bd3268c070fb430a6b2a651
ravilella1234 <masters.selenium@gmail.com>
2022-12-20 08:55:47 +0530

    Java docs added

---
+++ December2022_8AMJavaBatch/src/icici/loans/homeloans/VariableTypes.java
@@ -0,0 +1,35 @@
+package icici.loans.homeloans;
+
+public class VariableTypes 
+{
+	// primitive
+	int x = 100; // instance (or) global (or) non-static
+	static String cname = "vmware"; // static (or) static-global
+	static VariableTypes obj = new VariableTypes();
+	
+	public void m1()
+	{
+		int x = 200; // local
+		//System.out.println(y);
+		System.out.println(x);
+		System.out.println(this.x);
+		System.out.println(VariableTypes.cname);
+	}
+	
+	public static void m2()
+	{
+		int z = 300;
+		//VariableTypes obj = new VariableTypes();
+		System.out.println(obj.x);
+		System.out.println(VariableTypes.cname);
+		System.out.println(z);
+	}
+	
+	public static void main(String[] args) 
+	{
+		//VariableTypes obj = new VariableTypes(); // non-primitive (or) reference
+		System.out.println(obj.x);
+		System.out.println(VariableTypes.cname);	
+	}
+
+}
