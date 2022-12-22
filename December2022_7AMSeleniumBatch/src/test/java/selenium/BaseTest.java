b0735d4e4ddc7e45cb4a1ab8baac4149bed06b8f
ravilella1234 <masters.selenium@gmail.com>
2022-12-22 08:24:30 +0530

    Locators Day-2

--- December2022_7AMSeleniumBatch/src/test/java/selenium/BaseTest.java
+++ December2022_7AMSeleniumBatch/src/test/java/selenium/BaseTest.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -18,6 +20,7 @@ public class BaseTest
 	public static Properties p;
 	public static Properties mainProp;
 	public static Properties childProp;
+	public static Properties orProp;
 	
 	public static void init() throws Exception
 	{
@@ -37,6 +40,10 @@ public class BaseTest
 		childProp.load(fis);
 		String val = childProp.getProperty("amazonurl");
 		System.out.println(val);
+		
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\or.properties");
+		orProp = new Properties();
+		orProp.load(fis);
 	}
 	
 	public static void launch(String browser)
@@ -57,6 +64,47 @@ public class BaseTest
 	{
 		driver.get(childProp.getProperty(url));
 		driver.navigate().to(childProp.getProperty(url));
+	}
+	
+	public static void clickElement(String locatorKey) 
+	{
+		//driver.findElement(By.xpath(orProp.getProperty(locatorKey))).click();
+		getElement(locatorKey).click();
+	}
+
+	public static void typeText(String locatorKey, String text) 
+	{
+		//driver.findElement(By.name(orProp.getProperty(locatorKey))).sendKeys(text);
+		getElement(locatorKey).sendKeys(text);
+	}
+
+	public static void selectOption(String locatorKey, String option) 
+	{
+		//driver.findElement(By.id(orProp.getProperty(locatorKey))).sendKeys(option);
+		getElement(locatorKey).sendKeys(option);
+	}
+	
+	public static WebElement getElement(String locatorKey) 
+	{
+		WebElement element = null;
+		
+		if(locatorKey.endsWith("_id")) {
+			element = driver.findElement(By.id(orProp.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_name")) {
+			element = driver.findElement(By.name(orProp.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_classname")) {
+			element = driver.findElement(By.className(orProp.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_xpath")) {
+			element = driver.findElement(By.xpath(orProp.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_css")) {
+			element = driver.findElement(By.cssSelector(orProp.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_linktext")) {
+			element = driver.findElement(By.linkText(orProp.getProperty(locatorKey)));
+		}else if(locatorKey.endsWith("_partiallinktext")) {
+			element = driver.findElement(By.partialLinkText(orProp.getProperty(locatorKey)));
+		}
+		
+		return element;
 	}
 
 }
---
+++ December2022_7AMSeleniumBatch/src/test/java/selenium/TC_003.java
@@ -0,0 +1,32 @@
+package selenium;
+
+public class TC_003 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigateUrl("amazonurl");
+		
+		driver.manage().window().maximize();
+		
+		selectOption("amazondropbox_id","Books");
+		
+		typeText("amazonsearchtext_name","Harry Potter");
+		
+		clickElement("amazonsearchbutton_xpath");
+		
+		
+		//WebElement loc = driver.findElement(By.id("searchDropdownBox"));
+		//loc.sendKeys("Books");
+		
+		//driver.findElement(By.id("searchDropdownBox")).sendKeys("Books");
+		
+		//driver.findElement(By.name("field-keywords")).sendKeys("Harry Potter");
+		
+		//driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).click();
+	}
+}
