af188e7140ec8d4a5d963e2a5d50572759478d18
ravilella1234 <masters.selenium@gmail.com>
2022-12-27 08:55:11 +0530

    Type Castings

---
+++ December2022_8AMJavaBatch/src/icici/loans/homeloans/WapperClassDemo.java
@@ -0,0 +1,16 @@
+package icici.loans.homeloans;
+
+public class WapperClassDemo
+{
+
+	public static void main(String[] args) 
+	{
+		int i1 = 10;
+		System.out.println(i1);
+
+		
+		Integer i2 = 20;
+		System.out.println(i2);
+	}
+
+}
